453c21130bd6e940945a14db3e93e50c2383b016
Aman Jeet Singh <amanjeetthakur644@gmail.com>
2026-03-04 17:21:06 +0530

    main.ts change

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -91,7 +91,8 @@ async function bootstrap() {
         SwaggerModule.setup('api/docs', app, document);
     }
 
-    const port = configService.get('PORT') || 3001;
+    // const port = configService.get('PORT') || 3001;
+    const port = process.env.PORT || 3000 || 3001;
     console.log(`Starting application on port ${port}...`);
     try {
         await app.listen(port, '0.0.0.0');
